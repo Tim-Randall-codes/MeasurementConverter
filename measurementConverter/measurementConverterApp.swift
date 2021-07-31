bb9b6f3f357f65c924fa090d0e36628cef2bca78
Tim Randall <trtimrandall@gmail.com>
2021-08-01 00:46:28 +1000

    Initial Commit

---
+++ measurementConverter/measurementConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  measurementConverterApp.swift
+//  measurementConverter
+//
+//  Created by Tim Randall on 1/8/21.
+//
+
+import SwiftUI
+
+@main
+struct measurementConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
