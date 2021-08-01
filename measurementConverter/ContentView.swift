3b0aafeb66fda792b4ee7ec8a0ec579ce2bc0941
Tim Randall <trtimrandall@gmail.com>
2021-08-01 17:28:02 +1000

    final

--- measurementConverter/ContentView.swift
+++ measurementConverter/ContentView.swift
@@ -8,9 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var heightInputString: String = ""
+    @State var weightInputString: String = ""
+    @State var convertToInches: Bool = true
+    @State var convertToPounds: Bool = true
+    @State var heightOutput: Float = 0
+    @State var weightOutput: Float = 0
+    @State var heightDisplay: String = ""
+    @State var weightDisplay: String = ""
+    @State var feetInchDisplay: String = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Measurement Converter").fontWeight(.heavy)
+                .padding()
+            Spacer()
+            HStack{
+                TextField("Enter height", text: $heightInputString)
+                Toggle("Cm to Inches (green) or Inches to Cm?", isOn: $convertToInches)
+            }
+            HStack{
+                Text(heightDisplay)
+                Spacer()
+                Button("Convert", action:{
+                    heightConverter()
+                })
+            }
+            Text(feetInchDisplay)
+                .padding()
+            Spacer()
+            HStack{
+                TextField("Enter weight", text: $weightInputString)
+                Toggle("Kg to pounds or pounds to kg (green)", isOn: $convertToPounds)
+            }
+            HStack{
+                Text(weightDisplay)
+                Spacer()
+                Button("Convert", action:{
+                    weightConverter()
+                })
+            }
+            Spacer()
+        }
+    }
+    func heightConverter() {
+        let heightInput: Float = Float(heightInputString) ?? 0
+        if convertToInches == true {
+            let heightDisplayFloat = heightInput * 0.393701
+            heightDisplay = String(heightDisplayFloat)
+            let heightInt: Int = Int(heightDisplayFloat)
+            let heightFeet: Int = heightInt / 12
+            let heightInches: Int = heightInt % 12
+            feetInchDisplay = "\(heightFeet) feet and \(heightInches) inches"
+        }
+        else {
+            let heightDisplayFloat = heightInput * 2.54
+            heightDisplay = String(heightDisplayFloat)
+            feetInchDisplay = ""
+        }
+    }
+    func weightConverter() {
+        let weightInput: Float = Float(weightInputString) ?? 0
+        if convertToPounds == true {
+            let weightDisplayFloat = weightInput * 0.453592
+            weightDisplay = String(weightDisplayFloat)
+            feetInchDisplay = ""
+        }
+        else {
+            let weightDisplayFloat = weightInput * 2.20462
+            weightDisplay = String(weightDisplayFloat)
+            feetInchDisplay = ""
+        }
     }
 }
 
